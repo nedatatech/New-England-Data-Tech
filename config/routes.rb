6a3c123b24ce663982c754830a41f9a9d23a78f5
Tim M <poad27xx@gmail.com>
2018-01-23 13:45:31 -0500

    added files missing from prev commit

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'services', to: 'home#services'
-  get 'gallery', to: 'photos#index'
+  get 'gallery', to: 'images#index'
   get 'contact', to: 'home#contact'
 end
