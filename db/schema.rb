8fe0a23ec7c4c4e3b32594f0aa58311ee7fefb2d
Tim M <poad27xx@gmail.com>
2018-01-29 16:08:32 -0500

    Start reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124210026) do
+ActiveRecord::Schema.define(version: 20180129201456) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20180124210026) do
     t.string   "file"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
