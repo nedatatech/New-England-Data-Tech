d27116a57c1e76b1fbb2754485a9a6ce905c9f92
Tim M <poad27xx@gmail.com>
2018-02-16 23:41:49 -0500

    Removed old models that caused errors with passenger trying to load site (photos, gallerys)

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,8 @@ Rails.application.routes.draw do
   get '/albums/new', to: 'albums#new'
   resources :users
   resources :sessions
-  resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'services', to: 'home#services'
-  get 'gallery', to: 'images#index'
   get 'contact', to: 'home#contact'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,14 +32,6 @@ ActiveRecord::Schema.define(version: 20180131020920) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "file"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.float    "rating"
     t.string   "first_name"
